4a435f69e11fa06d0430681807783d15e2fea677
Philip Vejre <philipvejre@gmail.com>
2020-07-15 14:53:22 +0200

    Remove time dependency

--- src/src/main.rs
+++ src/src/main.rs
@@ -1,7 +1,9 @@
-//! Cryptagraph is a tool for finding linear approximations and differentials of block ciphers. 
+//! Cryptagraph is a tool for finding linear approximations and differentials of block ciphers.
 
-#[macro_use] extern crate lazy_static;
-#[macro_use] extern crate structopt_derive;
+#[macro_use]
+extern crate lazy_static;
+#[macro_use]
+extern crate structopt_derive;
 
 extern crate crossbeam_utils;
 extern crate fnv;
@@ -10,74 +12,76 @@ extern crate itertools;
 extern crate num_cpus;
 extern crate rand;
 extern crate structopt;
-extern crate time;
 
-mod options;
 pub mod cipher;
 pub mod dist;
+mod options;
 pub mod property;
 pub mod sbox;
 pub mod search;
 pub mod utility;
 
+use crate::cipher::*;
 use crate::options::CryptagraphOptions;
 use structopt::StructOpt;
-use crate::cipher::*;
 
 fn main() {
     match CryptagraphOptions::from_args() {
-        CryptagraphOptions::Search {cipher, 
-                                    property_type, 
-                                    rounds, 
-                                    num_patterns, 
-                                    anchors, 
-                                    file_mask_in, 
-                                    file_mask_out, 
-                                    num_keep,
-                                    file_graph} => {
-            
+        CryptagraphOptions::Search {
+            cipher,
+            property_type,
+            rounds,
+            num_patterns,
+            anchors,
+            file_mask_in,
+            file_mask_out,
+            num_keep,
+            file_graph,
+        } => {
             let cipher = match name_to_cipher(cipher.as_ref()) {
                 Some(c) => c,
-                None    => {
+                None => {
                     println!("Cipher not supported. Check --help for supported ciphers.");
                     return;
                 }
             };
 
             search::search_properties::search_properties(
-                cipher.as_ref(), 
-                property_type, 
-                rounds, 
-                num_patterns, 
+                cipher.as_ref(),
+                property_type,
+                rounds,
+                num_patterns,
                 anchors,
-                file_mask_in, 
-                file_mask_out, 
-                num_keep, 
-                file_graph);
-        },
-        CryptagraphOptions::Dist {cipher,
-                                  file_mask_in,
-                                  rounds,
-                                  keys,
-                                  masks,
-                                  output} => {
-            
+                file_mask_in,
+                file_mask_out,
+                num_keep,
+                file_graph,
+            );
+        }
+        CryptagraphOptions::Dist {
+            cipher,
+            file_mask_in,
+            rounds,
+            keys,
+            masks,
+            output,
+        } => {
             let cipher = match name_to_cipher(cipher.as_ref()) {
                 Some(c) => c,
-                None    => {
+                None => {
                     println!("Cipher not supported. Check --help for supported ciphers.");
                     return;
                 }
             };
 
             dist::distributions::get_distributions(
-                cipher.as_ref(), 
-                &file_mask_in, 
-                rounds, 
-                keys, 
-                &masks, 
-                &output);
+                cipher.as_ref(),
+                &file_mask_in,
+                rounds,
+                keys,
+                &masks,
+                &output,
+            );
         }
     }
 }
-    
